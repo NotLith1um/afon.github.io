4b4ef69b453f3d9d046f083b38c5a095b63d858f
NotLith1um <95568058+NotLith1um@users.noreply.github.com>
2022-08-03 17:48:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,72 @@
+
 function logar(){
-    var usuario = document.getElementById("usuario");
-    var senha = document.getElementById("senha");
+        var usuario = document.getElementById("usuario");
+        var senha = document.getElementById("senha");
 
-    if(usuario.value == "admin@admin.com" && senha.value == "admin"){
-    localStorage.setItem(acesso, true);
+        if(usuario.value == "admin@admin.com" && senha.value == "admin"){
+        localStorage.setItem(acesso, true);
 
-    window.location.href = "index.html";
+        window.location.href = "principal.html";
+
+        }
+        else{
+            alert("Usuário ou senha inválidos!")
+        }
+}
+
+
+function registrar(){
+    var usuario = document.getElementById("regusuario");
+    var email = document.getElementById("regemail")
+    var senha = document.getElementById("regsenha");
+
+    if(usuario.value != "" || senha.value != "" || email.value != ""){
+    localStorage.setItem(confirma, true);
+
+    window.location.href = "principal.html";
 
     }
     else{
         alert("Usuário ou senha inválidos!")
     }
+}
+
+// Contatos
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+function openModal(modal){
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function closeModal(modal){
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
 }
